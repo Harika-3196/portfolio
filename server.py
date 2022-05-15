08c889aab07fffbfc2cc48e5e09b86ffc13fecc1
h0s060n <h0s060n@homeoffice.wal-mart.com>
2022-05-15 15:14:19 +0530

    added resume

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('portfolio.html')
+
+@app.route('/personal_data/<path:path>')
+def personal_data(path):
+   return send_from_directory("personal_data",path)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5001)
